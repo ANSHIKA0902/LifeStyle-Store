8be84b7aa87a789572fa40f021ab8908644bc730
ANSHIKA0902 <75514933+ANSHIKA0902@users.noreply.github.com>
2020-12-10 18:04:30 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+
+
+<footer class="footer">
+	<div class="container">
+	<center>
+	   <p>Copyright Anshika Mankotia &copy Lifestyle Store. All Rights Reserved. | Contact Us: +91 70185 73711</p>
+	</center>
+	</div>
+
+</footer>
